b316e98d7f6dab652a0d3f6c337cc38c91740d63
simuelunbo <simuelunbo@gmail.com>
2025-03-18 20:46:41 +0900

    feat: domain 모듈 Model 추가
    - GardenPlant, Plant Model 추가

---
+++ core/domain/src/main/java/com/simuel/sunflower/core/domain/model/GardenPlant.kt
@@ -0,0 +1,9 @@
+package com.simuel.sunflower.core.domain.model
+
+data class GardenPlant (
+    val plantId: String,
+    val plantName: String,
+    val plantedDate: String,
+    val lastWateringDate: String,
+    val wateringInterval: Int
+)
