86eed94099df7b234848f2ce6028274b253cfa96
simuelunbo <simuelunbo@gmail.com>
2025-03-20 02:52:07 +0900

    feat: 상세화면에 따른 상태 Viewmodel 로직 추가

--- feature/plantdetail/src/main/java/com/simuel/sunflower/feature/plantdetail/PlantDetailViewModel.kt
+++ feature/plantdetail/src/main/java/com/simuel/sunflower/feature/plantdetail/PlantDetailViewModel.kt
@@ -1,9 +1,30 @@
 package com.simuel.sunflower.feature.plantdetail
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.simuel.sunflower.core.domain.repository.PlantDetailRepository
+import com.simuel.sunflower.feature.plantdetail.model.PlantDetailUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class PlantDetailViewModel @Inject constructor(
-) : ViewModel()
+    private val repository: PlantDetailRepository
+) : ViewModel() {
+    private val _uiState = MutableStateFlow<PlantDetailUiState>(PlantDetailUiState.Loading)
+    val uiState: StateFlow<PlantDetailUiState> = _uiState.asStateFlow()
+
+    fun loadPlantDetail(plantId: String) {
+        _uiState.value = PlantDetailUiState.Loading
+
+        viewModelScope.launch {
+            val plant = repository.getPlantDetail(plantId)
+            _uiState.value = PlantDetailUiState.Success(plant)
+        }
+    }
+}
