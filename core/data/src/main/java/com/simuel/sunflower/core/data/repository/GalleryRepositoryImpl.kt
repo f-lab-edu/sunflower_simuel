928b16903e82315ee30314f974355e79f7d591cc
simuelunbo <simuelunbo@gmail.com>
2025-03-28 00:01:04 +0900

    refactor: 코드 베이스 일관성을 위해 컨버전 컨스트럭터로 호출 처리

--- core/data/src/main/java/com/simuel/sunflower/core/data/repository/GalleryRepositoryImpl.kt
+++ core/data/src/main/java/com/simuel/sunflower/core/data/repository/GalleryRepositoryImpl.kt
@@ -1,6 +1,5 @@
 package com.simuel.sunflower.core.data.repository
 
-import com.simuel.sunflower.core.data.model.PhotoDto
 import com.simuel.sunflower.core.data.source.RemoteGalleryDataSource
 import com.simuel.sunflower.core.domain.model.Photo
 import com.simuel.sunflower.core.domain.repository.GalleryRepository
@@ -9,28 +8,20 @@ import javax.inject.Inject
 internal class GalleryRepositoryImpl @Inject constructor(
     private val remoteGalleryDataSource: RemoteGalleryDataSource
 ) : GalleryRepository {
-    
+
     override suspend fun getCategoryPhotos(
-        category: String, 
-        page: Int, 
-        pageSize: Int
+        category: String, page: Int, pageSize: Int
     ): List<Photo> {
         return remoteGalleryDataSource.fetchPhotosByCategory(
-            query = category,
-            page = page,
-            perPage = pageSize
+            query = category, page = page, perPage = pageSize
         ).map { photoDto ->
-            photoDto.toDomainModel()
+            Photo(
+                id = photoDto.id,
+                url = photoDto.url,
+                name = photoDto.name,
+                userName = photoDto.userName,
+                description = photoDto.description
+            )
         }
-    }
-    
-    private fun PhotoDto.toDomainModel(): Photo {
-        return Photo(
-            id = id,
-            url = url,
-            name = name,
-            userName = userName,
-            description = description
-        )
     }
 }
